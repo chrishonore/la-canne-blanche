dd6671f8c2551646f6a836df8655632e6c476608
Anthony Velez <josuevelez@outlook.es>
2024-03-28 13:52:33 +0100

    ajout modif

--- DB/db_user.sql
+++ DB/db_user.sql
@@ -1,13 +1,16 @@
-     CREATE DATABASE IF NOT EXISTS  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+     CREATE DATABASE PhilipflixIF NOT EXISTS  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-            use ;
+            use PhilipflixIF;
 CREATE TABLE IF NOT EXISTS User
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(30) NOT NULL,
-    LastName VARCHAR(30) NOT NULL,
+    nom VARCHAR(30) NOT NULL,
+    prenom VARCHAR(30) NOT NULL,
+    dateNaissance  DATE NOT NULL,
+    username VARCHAR(30) NOT NULL,
     Email VARCHAR(50) NOT NULL,
     password VARCHAR(200) NOT NULL,
+    passwordFonfVARCHAR(200) NOT NULL,
     Birthday DATE NOT NULL
 
 );
