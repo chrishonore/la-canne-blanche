2b9cb03d86fa8df4400da2d526c6aceb445d6023
unknown <christianhonore@outlook.com>
2024-03-28 10:11:30 +0100

    script

--- script/signIn.php
+++ script/signIn.php
@@ -5,11 +5,13 @@
 require('config.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nom=$_POST[ 'nom'];
+    $prenom=$_POST['prenom'];
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    $query = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    $query = "INSERT INTO users(nom,prenom,username,email,password)VALUES('$username','$email', '$password')";
     $conn = 1;
     $result = mysqli_query($query, $conn);
 
