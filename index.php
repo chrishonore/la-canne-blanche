3ace23d72c413cb019244ccf9ffced9279ce7f25
FilyDev <filymuratore@gmail.com>
2024-03-28 21:57:50 +0100

    update

--- index.php
+++ index.php
@@ -12,8 +12,10 @@ if (isset($_GET['page'])) $page = $_GET['page'];
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="assets/css/app.css">
     <link rel="stylesheet" href="assets/css/fonts.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo getPageTitle($page) ?></title>
 </head>
@@ -23,5 +25,6 @@ if (isset($_GET['page'])) $page = $_GET['page'];
         include 'router.php';
         include 'layouts/footer.php';
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
