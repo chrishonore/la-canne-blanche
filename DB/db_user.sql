ad6898acffba45df4a4101518be8c98a9aeb0ff4
Carmen T <carmentodorutz@yahoo.com>
2024-03-28 14:32:28 +0100

    Petites correction des champs db_user

--- DB/db_user.sql
+++ DB/db_user.sql
@@ -1,16 +1,15 @@
-     CREATE DATABASE PhilipflixIF NOT EXISTS  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+     CREATE DATABASE Philipflix IF NOT EXISTS  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-            use PhilipflixIF;
+            use Philipflix;
 CREATE TABLE IF NOT EXISTS User
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(30) NOT NULL,
     prenom VARCHAR(30) NOT NULL,
     dateNaissance  DATE NOT NULL,
+    email VARCHAR(50) NOT NULL,
     username VARCHAR(30) NOT NULL,
-    Email VARCHAR(50) NOT NULL,
     password VARCHAR(200) NOT NULL,
-    passwordFonfVARCHAR(200) NOT NULL,
-    Birthday DATE NOT NULL
+    passwordConf VARCHAR(200) NOT NULL,
 
 );
