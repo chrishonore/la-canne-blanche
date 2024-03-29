ce428e65b942accf69eb201e0a242d73f250bf72
Fily <fily@MacBook-Pro-de-Fily.local>
2024-03-29 10:51:11 +0100

    update dashboard design

--- layouts/footer.php
+++ layouts/footer.php
@@ -1,4 +1,4 @@
 <footer class="w-100 px-5 pt-3">
-    <p class="float-start">Philipflix © 2024</p>
-    <p class="float-end">Tout droit réservé.</p>
+    <p class="float-start pb-4">Philipflix © 2024</p>
+    <p class="float-end pb-4">Tout droit réservé.</p>
 </footer>
--- views/dashboard.php
+++ views/dashboard.php
@@ -1,44 +1,43 @@
 <div class="login">
-    <h1>Le dashboard de l'admin</h1>
-    <p>Veuillez enregistrer les series que vous voulez repertorier dans le site.</p>
     <form name="addSeries" method="post" action="?page=dashboard" enctype="multipart/form-data">
-           <div class="row">
-            <label for="title">Title de la serie :</label>
-            <input type="text" name="title" id="title" placeholder="Champ obligatoire" required autofocus>
+        <fieldset class="mt-5 col-xl-4 col-lg-5 col-md-6 col-sm-9 col-9 m-auto">
+            <legend class="text-center mb-5">Ajouter un film</legend>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="title">Title de la serie :</label>
+                <input class="form-control bg-transparent text-white ps-3 py-3" type="text" name="title" id="title" placeholder="Champ obligatoire" required autofocus>
            </div>
-        <div class="row">
-            <label for="description">Description :</label>
-            <input type="text" name="description" id="description" placeholder="Champ obligatoire" required autofocus>
-        </div>
-        <div class="row">
-            <label for="image">Choisissez une image :</label>
-            <input type="file" id="image" name="image" alt="">
-        </div>
-        <div class="row">
-            <label for="Lien_trailer">Inserez le lien du trailer :</label>
-            <input type="url" name="Lien_trailer" id="Lien_trailer" >
-        </div>
-        <div class="row">
-            <label for="reference"> NETFLIX :</label>
-            <label for="link"></label><input type="checkbox" name="netflix" id="link" >
-        </div>
-        <div class="row">
-            <label for="reference"> AppelTv :</label>
-            <input type="checkbox" name="AppelTv" id="link" >
-        </div>
-        <div class="row">
-            <label for="reference"> PRIME :</label>
-            <input type="checkbox" name="PRIME" id="link" >
-        </div>
-        <div class="row">
-            <label for="reference"> DisneyPlus :</label>
-            <input type="checkbox" name="DisneyPlus" id="link" >
-        </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="description">Description :</label>
+                <textarea class="form-control bg-transparent text-white ps-3 py-3" name="description" id="description" placeholder="Champ obligatoire"></textarea>
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="image">Inserez le lien vers une image :</label>
+                <input class="form-control bg-transparent text-white ps-3 py-3" type="url" name="image" id="image" placeholder="Champ obligatoire">
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="Lien_trailer">Inserez le lien du trailer :</label>
+                <input class="form-control bg-transparent text-white ps-3 py-3" type="url" name="Lien_trailer" id="Lien_trailer" placeholder="Champ obligatoire">
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="netflix"> NETFLIX</label>
+                <input type="checkbox" name="netflix" id="netflix" >
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="AppelTv"> APPLE TV</label>
+                <input class="ps-3 py-3" type="checkbox" name="AppelTv" id="AppelTv" >
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="prime"> PRIME</label>
+                <input class="ps-3 py-3" type="checkbox" name="prime" id="prime" >
+            </div>
+            <div class="form-group m-auto mb-3">
+                <label class="ps-3 py-1" for="DisneyPlus"> DISNEY+</label>
+                <input class="ps-3 py-3" type="checkbox" name="DisneyPlus" id="DisneyPlus" >
+            </div>
 
-        <div class="row">
-        <input type="submit" name="register" id="register" value="Enregistrez">
-        <input type="reset" name="Annuler" id="Annuler" value="Annuler">
-        </div>
+            <input class="btn btn-outline-success me-3" type="submit" name="register" id="register" value="Ajouter">
+            <input class="btn btn-outline-warning" type="reset" name="Annuler" id="Annuler" value="Annuler">
+        </fieldset>
     </form>
 </div>
 <?php
