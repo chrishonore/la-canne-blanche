8851547b4d2264477d06f72e2985f101c5c84815
Carmen T <carmentodorutz@yahoo.com>
2024-03-29 10:08:44 +0100

    .

--- DB/db_user.sql
+++ DB/db_user.sql
@@ -4,13 +4,15 @@
 CREATE TABLE IF NOT EXISTS User
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(30) NOT NULL,
-    prenom VARCHAR(30) NOT NULL,
-    dateNaissance  DATE NOT NULL,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    birthday  DATE NOT NULL,
     email VARCHAR(50) NOT NULL,
     username VARCHAR(30) NOT NULL,
     password VARCHAR(200) NOT NULL,
     passwordConf VARCHAR(200) NOT NULL,
-INSERT INTO User(Storme, Anna, anna@gmail.com, 12.12.2009, anabanana, 12345Asdf, 12345Asdf)
+
+    INSERT INTO User (firstName, lastName, birthday, email, username, password, passwordConf)
+VALUES (Storme, Anna, anna@gmail.com, 12.12.2009, anabanana, 12345Asdf, 12345Asdf);
 
 );
--- script/signUp.php
+++ script/signUp.php
@@ -1,87 +1,19 @@
 <?php
-<<<<<<< HEAD
-$dbName = "Philipflix";
-=======
-<<<<<<< HEAD
-// Donnée de connexion à DB
-$dbName = "users";
-$dbUser = "username";
-$dbPassword = "password";
-$dbHost = "localhost";// dans votre cas : localhost
-try {
-    // Connexion à la DB
-    $bdd = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUser , $dbPassword);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion à la base de donnée réussie";
-} catch (PDOException $e) {
-    // Affichage des erreurs si jamais il y en a
-    exit("Erreur de connexion : " . $e->getMessage());
-}
-// Assignation des valeurs récupérées du front aux variables
-$username = $_POST["username"];
-$password = $_POST["password"];
-
-
-// Requête SQL pour vérifier l'utilisateur
-$query = "SELECT * from db_user_ WHERE username = :username";
-// Préparation de la requête
-$statement = $bdd->prepare($query);
-// Liaison de la requêtes avec les données récupérées dans le champ de formulaire
-$statement->bindParam(':username', $username);
-// Execution de la requête
-$statement->execute();
-// Récupération des données trouvée
-$user = $statement->fetch(PDO::FETCH_ASSOC);
-
-
-// Si $user est vide, la requête n'a rien trouvé dans la DB
-if ($user) {
-    // Vérification du mot de passe, si il correspond entre le mot de passe du champ de formulaire front et le mot de passe trouvé dans la DB avec notre requête
-    if ($password==$user['Password']) {
-        echo 'Connexion réussie !'; // Ou rediriger vers une page de succès
-    } else {
-        echo 'Mot de passe incorrect.';
-    }
-} else {
-    // Dans le else, tu dois mettre le comportement si jamais le nom d'utilisateur n'existe pas dans la base de donnée. Ici je l'ajoute, mais vous, ça sera peut-être autre chose !
-
-
-
-    // Préparation de la requête d'insertion
-    $requete = $bdd->prepare("INSERT INTO login_password (Username, Password) VALUES (:username, :password)");
-
-
-// Liaison des valeurs aux paramètres de la requête
-    $requete->bindParam(':username', $username);
-    $requete->bindParam(':password', $password);
-
-
-// Exécution de la requête
-    $requete->execute();
-    echo "utilisateur ajouté";
-}
-
-
-
-
-?>
-=======
 $dbName = "philipflix";
->>>>>>> d010e0ff135857cda669b86c9c2ded73a5b11b5c
 $dbUser = "root";
 $dbPassword = "johnnyestbeau";
 $dbHost = "localhost";
 try {
     $bdd = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName, $dbUser , $dbPassword);
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }
-    catch (PDOException $e) {
+}
+catch (PDOException $e) {
     exit("Erreur de connexion : " . $e->getMessage());
 }
 
-$nom = $_POST["nom"];
-$prenom = $_POST["prenom"];
-$dateNaissance = $_POST["dateNaissance"];
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$birthday = $_POST["birthday"];
 $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
@@ -126,4 +58,3 @@ if ($user) {
     }
 }
 ?>
->>>>>>> a0206771b84580cd6e798252af53b16fc1b8573d
